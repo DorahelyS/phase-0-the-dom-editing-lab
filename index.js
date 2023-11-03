280c4fa801759ba7634822d64f38c081b7d35f42
Dorahely Sanchez <dorahelys@gmail.com>
2023-11-03 14:30:24 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,9 @@
+//---------------- structure content of the DOM -----------------------
+
+// more often have starting and ending tags
+// EX: <body></body>
+// can nest the - when nested, inner is called child and outer is called the parent node
+//some only have self closing tag
+// EX: <img/>
+// * NOT / is optional can do just <img>
+
